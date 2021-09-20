230a6de9a9610755991e075c2b83219ed8e402f7
Aatish Pandey <32216307+aatishpandey@users.noreply.github.com>
2021-09-20 20:32:57 +0530

    update isTriangle.js

--- scripts/isTriangle.js
+++ scripts/isTriangle.js
@@ -8,18 +8,28 @@ function CalsulateSumOfAngles(angle1,angle2,angle3){
 }
 
 function isTriangle(){
-    const sumOfAngles = CalsulateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
-    console.log(sumOfAngles);
 
-    if(sumOfAngles === 180)
-    {
-        outputE1.innerText = "Yay , the angles form  a triangle";
-        console.log('Yay , the angles form  a triangle');
-    }
-    else{
-        outputE1.innerText = "Oh not forming a triangle!";
-        console.log('Oh not forming a triangle!');
-    }
+    const angle1 = Number(inputs[0].value);
+    const angle2 = Number(inputs[1].value);
+    const angle3 = Number(inputs[2].value);
+
+    if(angle1 && angle2 && angle3){
+        const sumOfAngles = CalsulateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
+        console.log(sumOfAngles);
+
+        if(sumOfAngles === 180)
+        {
+            outputE1.innerText = "Yay , the angles form  a triangle";
+            console.log('Yay , the angles form  a triangle');
+        }
+        else{
+            outputE1.innerText = "Oh not forming a triangle!";
+            console.log('Oh not forming a triangle!');
+        }
+        }else{
+            outputE1.innerText = "Please enter all values";
+        }
+
 }
 
 isTriangleBtn.addEventListener("click",isTriangle);
